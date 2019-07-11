4e33ce481707da8f94e0577fa7d458e353b27def
April Liu <ywang348@usfca.edu>
2019-07-11 13:04:22 -0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python get-pip.py
+sudo pip install virtualenv
+python -m virtualenv gwc_ds
+cd gwc_ds
+git clone https://github.com/aprilyichenwang/GirlWhoCode
+
+source bin/activate
+pip install -r GirlWhoCode/requirements.txt
+deactivate
